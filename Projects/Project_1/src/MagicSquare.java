4236f1f68799102db80d0ad7dba675fb33879553
Jacob Phillips (jacobphillip) <jacobphillip@micron.com>
2016-07-06 18:35:32 -0600

    Pushing new changes to project files

--- Projects/Project_1/src/MagicSquare.java
+++ Projects/Project_1/src/MagicSquare.java
@@ -1,4 +1,8 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /* C L A S S   D E F I N I T I O N
@@ -26,7 +30,19 @@ import java.util.Scanner;
  */
 public class MagicSquare {
 	
-	//private String filename
+
+	/*
+	 *  ============================================
+	 *    		    V A R I A B L E S 
+	 *  ============================================
+	 */
+	
+	static int[][] newMagicSquare;
+	static int squareSize;
+	static int magicNumber;
+	static String filename;
+
+	
 	
 	/*
 	 *  ============================================
@@ -37,9 +53,7 @@ public class MagicSquare {
 		
 	}
 	
-	
-	
-	
+
 	/*
 	 *  ============================================
 	 *  	  P U B L I C     M E T H O D S 
@@ -55,9 +69,14 @@ public class MagicSquare {
 	 */
 	public static void checkMatrix(String filename){
 		
+		
+		System.out.println("\n=============READING MATRIX===============\n");
+		
 		MagicSquare.readMatrix(filename);
 		
 	}
+	
+	
 	/*
 	 * createMatrix() 
 	 * This method checks to ensure that a given matrix is
@@ -70,12 +89,88 @@ public class MagicSquare {
 	 * 	@int size - size of the new magic square
 	 * 
 	 */
-	public static void createMatrix(String filename, int size){
+	public static void createMatrix(String filenameInput, int size){
+		System.out.println("\n============CREATING MATRIX==============\n");
+//		Create an integer value n = size of the Magic Square.
+//			Create a two-dimensional array of size n.
+//			Create two integer values, row and col.
+//			Set row = n - 1.
+//			Set col = n / 2.
+//			Create two integer values, old-row and old-col.
+//			Loop through the array from index i = 1 to i = n2
+//				Place the value of i in the array at the location [row][col].
+//				Set old-row = row and old-col = col.
+//				Increment row and col.
+//				If row == n, then replace it with 0.
+//				If col == n, then replace it with 0.
+//				Check the value stored at the location [row][col].
+//					If the element at [row][col] has already been filled,
+//						Then set row = old-row and col = old-col,
+//						And decrement row.
+		
+		//Attach values to global variables
+		filename = filenameInput;
+		squareSize = size;
+		magicNumber = (squareSize * ((squareSize * squareSize) + 1 )) / 2;
+		
+		System.out.println("Filename: " + filename);
+		System.out.println("Size : " + size);	
+		
+		int n = size;
+		
+		newMagicSquare = new int[n][n];
+		
+		int row;
+		int col;
+		int oldRow;
+		int oldCol;
+		
+		row = n - 1;
+		col = n / 2;
+		
+		for(int i = 1; i <= n * n; i++)
+		{
+			newMagicSquare[row][col] = i;	
+			oldRow = row;
+			oldCol = col;
+			row++;
+			col++;
+			
+			if(row == n){
+				row = 0;
+			}
+			if(col == n){
+				col = 0;
+			}
+			
+			if(newMagicSquare[row][col] > 0){
+				row = oldRow;
+				col = oldCol;
+				row--;
+			}
+		}
 		
 		
+		System.out.println(Arrays.deepToString(newMagicSquare));
 		
+		//PERFORM ALL CHECKS
+		if(checkSeries()){
+			if(checkRowColumnDiag()){
+				isMagicSquareAlert();
+					try {
+						writeMatrix();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+			}else{
+				isNotMagicSquareAlert();
+			}
+		}else
+		{
+			isNotMagicSquareAlert();
+		}
 	}
-	
 	
 	
 	/*
@@ -83,7 +178,8 @@ public class MagicSquare {
 	 *  	 P R I V A T E      M E T H O D S 
 	 *  ============================================
 	 */
-	private static void readMatrix(String filename){
+	private static void readMatrix(String filenameInput){
+		filename = filenameInput;
 		
 		try {
 			//String filename = "valid3x3.txt";
@@ -92,21 +188,200 @@ public class MagicSquare {
 			File file = new File(filename);
 			Scanner input = new Scanner(file);
 			
+			squareSize =  Integer.parseInt(input.nextLine());
+			
+			magicNumber = (squareSize * ((squareSize * squareSize) + 1 )) / 2;
+			
+			System.out.println("Magic number is: " + magicNumber + "\n");
+
+			newMagicSquare = new int[squareSize][squareSize];
+			
+			int iteratorIndex = 0;
+			
 			while (input.hasNextLine()){
 				String line = input.nextLine();
-				System.out.println(line);
+				String[] strArr = line.split(" ");
+		
+				for(int i = 0; i < strArr.length; i++)
+				{
+					if(strArr[i].length() > 0){			
+						newMagicSquare[iteratorIndex][i] = Integer.parseInt(strArr[i]);							
+						//System.out.println(Integer.parseInt(strArr[i]));
+					}
+				}
+				
+				iteratorIndex++;
 			}
 			
+			System.out.println(Arrays.deepToString(newMagicSquare));
 			input.close();
 
 		}
 		catch (Exception ex){
 			ex.printStackTrace();
-		}
+		}		
+		
+		//PERFORM ALL CHECKS
+		if(checkSeries()){
+			if(checkRowColumnDiag()){
+				isMagicSquareAlert();
+			}else{
+				isNotMagicSquareAlert();
+			}
+		}else
+		{
+			isNotMagicSquareAlert();
+		}	
 	}	
 	
 	
 	
 	
+	/*
+	 *  ============================================
+	 *  	   C R E A T E    M E T H O D S 
+	 *  ============================================
+	 */	
 	
+	private static void writeMatrix() throws IOException{
+		
+		File file = new File(filename);
+		PrintWriter outfile = new PrintWriter(new FileWriter(file));
+		
+		outfile.println(squareSize);
+		
+		for (int i = 0; i < squareSize; i++) {
+			
+			String line = new String(); 
+			
+			for(int j = 0; j < squareSize; j++){
+				line += (newMagicSquare[i][j] + " ");
+			}
+			
+			outfile.println(line);
+		  }
+		
+		outfile.close();
+		
+	}
+	
+	
+	/*
+	 *  ============================================
+	 *  	   C H E C K   M E T H O D S 
+	 *  ============================================
+	 */	
+	
+	private static boolean checkSeries(){
+		
+		boolean returnValue = true;
+		
+		int[] seriesArray = new int[squareSize * squareSize];
+		int seriesArrayCounter = 0;
+		
+		for(int i = 0; i < squareSize;i++){
+			for(int j = 0; j < squareSize;j++){
+				seriesArray[seriesArrayCounter] = newMagicSquare[i][j];
+				//System.out.println(newMagicSquare[i][j]);
+				seriesArrayCounter++;
+			}
+		}
+			
+		Arrays.sort(seriesArray);
+		for(int x = 0; x < seriesArray.length; x++){
+			if(x + 1 != seriesArray[x]){
+				returnValue = false;
+				break;
+			}
+		}
+		
+		return returnValue;
+	}
+	
+	
+	private static boolean checkRowColumnDiag(){
+		
+		System.out.println("Checking against magic number: " + magicNumber );
+		
+		boolean returnValue = true;
+		
+		//CHECK ROW
+		for(int i = 0; i < squareSize; i++)
+		{
+			int sum = 0;
+			
+			for(int j = 0; j < squareSize; j++){
+				sum += newMagicSquare[i][j];	
+			}
+			
+			if(sum != magicNumber){
+				returnValue = false;
+				System.out.println("Failed at: \n\t * row");
+			}
+		}
+		
+		//CHECK COLUMN
+		for(int i = 0; i < squareSize; i++)
+		{
+			int sum = 0;
+			
+			for(int j = 0; j < squareSize; j++){
+				sum += newMagicSquare[j][i];	
+			}
+			
+			if(sum != magicNumber){
+				returnValue = false;
+				System.out.println("Failed at: \n\t * column");
+			}
+				
+		}
+		
+		//CHECK RIGHT DIAGNOAL
+		int rightSum = 0;
+		for(int i = 0; i < squareSize; i++)
+		{
+			rightSum += newMagicSquare[i][squareSize - i -1];			
+		}
+		if(rightSum != magicNumber){
+			returnValue = false;
+			System.out.println("Failed at: \n\t * right diagonal");
+		}
+		
+		
+		//CHECK LEFT DIAGNOAL
+		int leftSum = 0;
+		for(int i = 0; i < squareSize; i++)
+		{
+			leftSum += newMagicSquare[i][i];			
+		}
+		if(leftSum != magicNumber){
+			returnValue = false;
+			System.out.println("Failed at: \n\t * right diagonal");
+		}
+		
+		
+		return returnValue;
+	}
+	
+	
+	
+	/*
+	 *  ============================================
+	 *  	   U S E R    A L E R T S 
+	 *  ============================================
+	 */	
+	
+	static void isMagicSquareAlert(){
+		System.out.println("\n==========================================\n\n");
+		System.out.println("******************************************");
+		System.out.println("NOTE: We've got a Magic Square here!");
+		System.out.println("******************************************");
+	}
+	
+	static void isNotMagicSquareAlert(){
+		System.out.println("\n==========================================\n\n");
+		System.out.println("******************************************");
+		System.out.println("ALERT: This matrix is NOT a Magic Square!");
+		System.out.println("******************************************");
+	}
 }
--- Projects/Project_1/src/MagicSquareTester.java
+++ Projects/Project_1/src/MagicSquareTester.java
@@ -3,8 +3,7 @@ import java.util.ArrayList;
 public class MagicSquareTester {
 
 	public static void main(String[] args){
-		
-		
+			
 		//store all the arguments in different lists
 		ArrayList<String> options = new ArrayList<String>();
 		ArrayList<String> otherInputs = new ArrayList<String>();
@@ -28,7 +27,9 @@ public class MagicSquareTester {
 		//if there were more than two options, throw an error 
 		//and request they only enter in one option
 		if(options.size() > 1){
-			System.out.println("Try again, and only pass in one option, please");
+			MagicSquareTester.tooManyOptionsAlert();
+			
+			//EXIT PROGRAM
 			System.exit(0);
 		}
 		
@@ -38,7 +39,7 @@ public class MagicSquareTester {
 			
 			//Check to make sure there isn't more than one additional arg
 			if(otherInputs.size() > 1){
-				System.out.println("Sorry, you can only check one file at a time.");
+				MagicSquareTester.checkSquareAlert();
 				System.exit(0);
 			}
 			
@@ -54,42 +55,27 @@ public class MagicSquareTester {
 			
 			//Check to make sure there isn't more than one additional arg
 			if(otherInputs.size() != 2){
-				System.out.println("Sorry, you can only check one file at a time.");
+				
+				MagicSquareTester.createSquareAlert();
+				
+				//EXIT PROGRAM
 				System.exit(0);
 			}
 			
+			
 			int sizeIndex = MagicSquareTester.checkNum(otherInputs);
-			
-			
 			size = Integer.parseInt(otherInputs.remove(sizeIndex));
+			//Check to make sure the size is odd
+			if(size % 2 == 0){
+				System.out.println("Hold up! That\'s an even number. Enter an odd number please");
+				System.exit(0);
+			}
 			
 			filename = otherInputs.get(0);
 			
 			//create a new magic square
 			MagicSquare.createMatrix(filename, size);
 		}
-		
-		
-		
-		
-		for(int j = 0; j < options.size(); j++){
-			
-			System.out.println(options.get(j));
-			System.out.println(options.size());
-		}
-		
-//		//Loop through the command line arguments
-//		for(int i = 0; i < args.length; i++){
-//			System.out.print("Argument number:" + i);
-//			System.out.print(args[i] + "\n");
-//			
-//		}
-//		
-		
-		
-		//MagicSquare ms = new MagicSquare();
-		
-		
 		
 	}
 	
@@ -116,6 +102,38 @@ public class MagicSquareTester {
 		return index;
 	};
 	
+	private static void createSquareAlert(){
+		
+		System.out.println("Something's wrong with your input for creating a Magic Square.");
+		System.out.println("======I N S T R U C T I O N S======");
+		System.out.println("Please include: ");
+		System.out.println("\t1. ONE filename");
+		System.out.println("\t1. ONE odd integer");
+	};
+	
+	private static void checkSquareAlert(){
+		
+		System.out.println("Something's wrong with your input for checking a square.");
+		System.out.println("======I N S T R U C T I O N S======");
+		System.out.println("Please include: ");
+		System.out.println("\t1. ONE filename");
+	};
+	
+	private static void tooManyOptionsAlert(){
+		
+		System.out.println("Something's wrong with your input.");
+		System.out.println("=====I N S T R U C T I O N S=====");
+		//Check square
+		System.out.println("\nTO CHECK A SQUARE ENTER: ");
+		System.out.println("\t1. -check");
+		System.out.println("\t2. ONE filename (e.g. \"newSquare\")");
+		//Create square
+		System.out.println("\nTO CREATE A SQUARE ENTER: ");
+		System.out.println("\t1. -create ");
+		System.out.println("\t2. ONE filename (e.g. \"newSquare\")");
+		System.out.println("\t3. ONE odd integer (e.g. 5)");
+		
+	};
 	
 	
 	//Check to see if a number is parsable
